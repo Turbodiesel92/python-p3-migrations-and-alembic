fde69fecde0bd658dd8320961d039b4ae1fa7fd4
MatthewWilliams <turbodieseldodge@gmail.com>
2023-03-07 16:41:08 -0600

    migrations lab

---
+++ lib/migrations/versions/a11a42afa349_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a11a42afa349
+Revises: 
+Create Date: 2023-03-07 16:11:48.426003
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a11a42afa349'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
